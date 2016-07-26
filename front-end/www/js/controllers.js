97b33060cd0f632bc8e1277b72b017889dac02e6
IMUDGES-HYZ <1455723823@qq.com>
2016-07-26 19:27:10 +0800

    update createaccount

--- front-end/www/js/controllers.js
+++ front-end/www/js/controllers.js
@@ -5,9 +5,41 @@ angular.module('starter.controllers', [])
   $scope.b='';
   $scope.c=0;
   $.post("login.php",{id:$scope.a,pwd:$scope.b},function(){
-    
   })
+})
 
+/*以下为注册页面的ctrl*/
+
+.controller('CreateAccountCtrl',function ($scope) {
+  $scope.createemail='';
+  $scope.createpwd1='';
+  $scope.createpwd2='';
+  $scope.createerror=false;
+  $scope.incomplete=true;
+  $scope.$watch('createemail',function () {
+    $scope.test();
+  });
+  $scope.$watch('createpwd1',function () {
+    $scope.test();
+  });
+  $scope.$watch('createpwd2',function () {
+    $scope.test();
+  });
+  $scope.test=function () {
+    if($scope.createpwd1==$scope.createpwd2){
+      $scope.createerror=false;
+    }
+    else{
+      $scope.createerror=true;
+    }
+
+    if(!$scope.createemail.length||!$scope.createpwd1.length||!$scope.createpwd2.length){
+      $scope.incomplete=true;
+    }
+    else{
+      $scope.incomplete=false;
+    }
+  };
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
