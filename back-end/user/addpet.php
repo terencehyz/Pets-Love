f98fd56f6ef323ddbee1dfcd5af2b1cc3359ee54
IMUDGES-YY <929144090@qq.com>
2016-07-31 10:18:30 +0800

    2016年7月31日 10:18:27

--- back-end/user/addpet.php
+++ back-end/user/addpet.php
@@ -1,11 +1,43 @@
 <?php
 	require_once('../mysql/connect.php');
 	require_once('../mysql/lib/mysql-fun.php');
-	$hid="2";
-	$pname="宠物名";
-	$ptype="宠物种类";
-	$page="9";
-	$pabout="描述";
-	$psex="性别";
-	mysql_query("INSERT INTO pet (p_name,p_sex,p_type,p_age,p_about,host_id) values('$pname','$psex','$ptype','$page','$pabout','$hid')");
+	//前端传来用户的信息
+	$new['p_name']="1";
+	$new['p_sex']="1";
+	$new['p_type']="1";
+	$new['p_age']="1";
+	$new['host_id']="12";
+	$new['p_about']="1";
+	$new['p_detail_type']="1";
+	$sql="select * from pet where host_id=$new[host_id]";
+	$query=mysql_query($sql);
+	while($str=mysql_fetch_assoc($query)){
+		$all_pets[]=$str;
+	}
+	$length=count($all_pets);
+	$judge=0;;
+	//如果有重复的信息 将$judge置为1；
+	for($i=0;$i<$length;$i++){
+		if(array_intersect($all_pets[$i], $new)==$new){
+			$index=$i;
+			$judge=1;
+		}
+	}
+	//如果没有重复的信息 将执行插入操作并返回1;
+	if($judge==0){
+		$sql="insert into pet (p_name,p_sex,p_type,p_age,host_id,p_about,p_detail_type) values ('$new[p_name]','$new[p_sex]','$new[p_type]','$new[p_age]','$new[host_id]','$new[p_about]','$new[p_detail_type]')";
+		if(mysql_query($sql)){
+		//////////////////////////////////////$json_encode(value)
+		}
+		else{
+			$return =0;
+			$json=json_encode($return);
+			echo $json;
+		}
+	}
+	else {
+		$return=0;
+		$json=json_encode($return);
+		echo $json;
+	}
 ?>
--- back-end/user/login.php
+++ back-end/user/login.php
@@ -1,13 +1,14 @@
 <?php
-	//本文件向前端返回值为h_secret_key judge （judge==0的情况为密码错误）
+	//本文件向前端返回值为h_secret_key judge id（id可以在添加宠物的时候更快捷） 用户头像地址（judge==0的情况为密码错误）
 	require_once('../mysql/connect.php');
 	include_once('../mysql/lib/mysql-fun.php');
 	require_once('../lib/secret-key-fun.php');
+	require_once('../lib/json-fun.php');
 	//$array前端传输来的数据，此处为测试数据
 	//$json=file_get_contents('php://input',true);
 	//$array = json_decode($json,true);
-	$array['h_email']=$_POST['h_email'];
-	$array['h_password']=$_POST['h_password'];
+	$array['h_email']="yangyangds@imudges.com";//$_POST['h_email'];
+	$array['h_password']="123";//$_POST['h_password'];
 	$user_array=fetch_assoc("host","h_email",$array['h_email']);
 	$judge=0;
 	//此处的$array['h_password']为本文件中两次md5加密后的密码，在与前端对接完成后需改为一次md5加密
@@ -30,10 +31,11 @@
 		$user_message=query('host','h_email',$array['h_email']);
 		//登录成功向前端返回h_secret_key judge和本人头像位置信息信息
 		
-		$return ['judge']=1;
-		$return ['h_secret_key']=$hash;
-		$return ['h_photo']=$user_message['h_photo'];
-		$json= json_encode($return);
+		// $return ['judge']=1;
+		// $return ['h_secret_key']=$hash;
+		// $return ['h_photo']=$user_message['h_photo'];
+		// $return ['id']=$user_message['id'];
+		$json= ch_json_encode($user_message);
 		echo $json;
  	}
 	else{
