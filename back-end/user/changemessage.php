bb16dcde6d9e6b2d786810c98e45e8b44ce23545
IMUDGES-YY <929144090@qq.com>
2016-08-08 21:45:59 +0800

    2016年8月8日 21:45:58

--- back-end/user/changemessage.php
+++ back-end/user/changemessage.php
@@ -4,14 +4,12 @@
 	require_once('../mysql/lib/mysql-fun.php');
 	//$array_message 和h_secret_key为前端传来的数据 后期需要修改
 	$h_secret_key='a72e8a7df5bee53a92f07c58fc441760';
-	$array_message=array(
-		'h_name'=>'周炎',
+	$array_message=array(,
 		'h_sex'=>'男',
 		'h_call'=>'15184779853',
-		'h_location'=>'呼和浩特市',
 		'h_account'=>'zy'
 		);
-	$sql="update host set h_name='$array_message[h_name]' , h_sex='$array_message[h_sex]' , h_call='$array_message[h_call]' , h_location='$array_message[h_location]',h_account='$array_message[h_account]' where h_secret_key='$h_secret_key'";
+	$sql="update host set  h_sex='$array_message[h_sex]' , h_call='$array_message[h_call]' ,h_account='$array_message[h_account]' where h_secret_key='$h_secret_key'";
 	$query=mysql_query($sql);
 	if($query){
 		$return['judge'] =1;
--- back-end/user/searchpets.php
+++ back-end/user/searchpets.php
@@ -1,34 +1,22 @@
-<?php 
-	//本文件向前端返回一个二维数组，因为搜索结果可能为多个宠物的所有信息
-	include_once('../mysql/lib/mysql-fun.php');
+<?php
+	header( 'Access-Control-Allow-Origin:*' );
 	require_once('../mysql/connect.php');
-	//array_message 为前端传输来的数据
-	$array_message=array(
-		'p_sex'=>'雌性',
-		'p_type'=>'',
-		'p_age'=>'',
-		'h_location'=>''
-		);
-	if($array_message['h_location']){
-		//如果用户搜索宠物时填写了location信息
-		$h_message=query('host','h_location',$array_message['h_location']);
-		$h_id=$h_message['id'];
-		echo $h_id;
-		$pets_message=query('pet','host_id',$h_id);
-		$json=json_encode($pets_message);
-		echo $json;
-		//将$pets_message的信息传给前端
+	require_once('../mysql/lib/mysql-fun.php');
+	//$message由前端传来的搜索关键字
+	$message='';//$_GET['message'];
+	$sql="select * from pet where p_name like '%$message%' or p_type like '%$message%' or p_age like '%$message%' or p_about like '%$message%'";
+	$query=mysql_query($sql);
+	while($str=mysql_fetch_assoc($query)){
+		$search_message[]=$str;
 	}
-	else {
-		//如果用户未填写location信息
-		$sql="select * from pet where p_sex ='$array_message[p_sex]' or p_age='$array_message' or p_age='$array_message[age]'";
-		$query=mysql_query($sql);
-		while ($str = mysql_fetch_assoc($query)) {
-			$pets_message[]=$str;
-		}
-		$json=json_encode($pets_message);
+	if(empty($search_message)){
+		$json['judge']=json_encode(0);
 		echo $json;
-		//$pets_message是二维数组，访问示例$pets_message[0]['p_name'];
-	}	
-	mysql_close($con);
+	}
+	else{
+		$search_message[]['judge']=1;
+		$json=json_encode($search_message);
+		echo $json;
+	}
 ?>
+<!-- hupeng zuishuai -->
