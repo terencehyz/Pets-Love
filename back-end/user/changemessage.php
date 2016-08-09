bc7bf9885dcb7055f506879a8f6b9ed0d5fdb27a
IMUDGES-YY <929144090@qq.com>
2016-08-09 10:42:06 +0800

    2016年8月9日 10:42:04

--- back-end/user/changemessage.php
+++ back-end/user/changemessage.php
@@ -1,15 +1,18 @@
 <?php
 	//本文件用于修改用户个人信息，向前端返回1 或0
+	header( 'Access-Control-Allow-Origin:*' ); 
+ 	header( 'Access-Control-Allow-Origin:*' ); 
 	require_once('../mysql/connect.php');
 	require_once('../mysql/lib/mysql-fun.php');
 	//$array_message 和h_secret_key为前端传来的数据 后期需要修改
-	$h_secret_key='a72e8a7df5bee53a92f07c58fc441760';
-	$array_message=array(,
-		'h_sex'=>'男',
-		'h_call'=>'15184779853',
-		'h_account'=>'zy'
-		);
-	$sql="update host set  h_sex='$array_message[h_sex]' , h_call='$array_message[h_call]' ,h_account='$array_message[h_account]' where h_secret_key='$h_secret_key'";
+	// $h_secret_key='a72e8a7df5bee53a92f07c58fc441760';
+	$array_message=$_GET;
+		// array(,
+		// 'h_sex'=>'男',
+		// 'h_call'=>'15184779853',
+		// 'h_account'=>'zy'
+		// );
+	$sql="update host set  h_sex='$array_message[h_sex]' , h_call='$array_message[h_call]' ,h_account='$array_message[h_account]' where h_secret_key='$array_message[h_secret_key]'";
 	$query=mysql_query($sql);
 	if($query){
 		$return['judge'] =1;
@@ -21,5 +24,4 @@
 		$json=json_encode($return);
 		echo $json;
 	}
-	mysql_close($con);
 ?>
